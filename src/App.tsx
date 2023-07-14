b808eca5df390ae0fb6cadb6c20e7a3846d8551d
Leonardo Mendes <leomendesrch2005@gmail.com>
2023-07-14 12:51:37 -0300

    style: first commit whatssap display

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import GroupImage from './assets/PPImage.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='back-ground'></div>
+      <div className='chat-container'>
+        <div className='user-container'>
+          <div className="user-info">
+            <img src={GroupImage} className="user-image" alt='Userphoto'></img>
+            <div className="user-settings">
+              <img src={GroupImage} className="user-image" alt='Userphoto'></img>
+              <img src={GroupImage} className="user-image" alt='Userphoto'></img>
+              <img src={GroupImage} className="user-image" alt='Userphoto'></img>
+              <img src={GroupImage} className="user-image" alt='Userphoto'></img>
+            </div>
+          </div>
+        </div>
+
+        <div className='mensages-container'>
+
+        </div>
+      </div>
     </div>
   );
 }
